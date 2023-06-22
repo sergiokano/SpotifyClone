91ce3c4a4f5d224cf126db9e6f8e09e1fc7591c9
Sergio Cano <sergiocano.dev@gmail.com>
2023-06-22 09:14:13 +0200

    feat: start sidebar left

---
+++ components/Sidebar.tsx
@@ -0,0 +1,58 @@
+"use client";
+import { useMemo } from "react";
+
+import { usePathname } from "next/navigation";
+import { HiHome } from "react-icons/hi";
+import { BiSearch } from "react-icons/bi";
+
+import Box from "./Box";
+import SidebarItem from "./SidebarItem";
+
+interface SidebarProps {
+  children: React.ReactNode;
+}
+
+const Sidebar: React.FC<SidebarProps> = ({ children }) => {
+  const pathname = usePathname;
+  const routes = useMemo(
+    () => [
+      {
+        icon: HiHome,
+        label: "Home",
+        active: pathname !== "/search",
+        href: "/",
+      },
+      {
+        con: BiSearch,
+        label: "Search",
+        active: pathname !== "/search",
+        href: "/search",
+      },
+    ],
+    [pathname]
+  );
+  return (
+    <div className="flex h-full">
+      <div
+        className="
+            hidden 
+            md:flex
+            flex-col
+            gap-y-2
+            bg-black
+            h-full
+            w-[300px]
+            p-2"
+      >
+        <Box className="flex flex-col gap-y-4 px-5 py-4">
+          {routes.map((item) => (
+            <SidebarItem key={item.label} {...item} />
+          ))}
+        </Box>
+        <Box className="overflow-y-auto h-full">Son Library</Box>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
---
+++ components/SidebarItem.tsx
@@ -0,0 +1,5 @@
+const SidebarItem = () => {
+  return <div>Sidebar item</div>;
+};
+
+export default SidebarItem;
